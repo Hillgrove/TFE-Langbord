b7b834bc96e3f3d1378fe8e2234e71b1b5aa65b0
Jesper Højlund <github@hillgrove.dk>
2024-12-02 19:05:24 +0100

    feat: clean up code

--- SenseHAT/SenseHAT.py
+++ SenseHAT/SenseHAT.py
@@ -1,48 +1,49 @@
-# HUSK AT INSTALL SÅ VI KAN BRUGE SENSEHAT
-# from sense_hat import SenseHat
-import time
-import socket
+# HUSK AT INSTALLERE SenseHat på Rasberry Pi
 import json
+import time
+from socket import AF_INET, SO_BROADCAST, SOCK_DGRAM, SOL_SOCKET, socket
 
-# sense = SenseHat()
- 
- 
-while True:
-    
-    # UDP_IP = "255.255.255.255" 
-    UDP_IP = "192.168.1.255" 
-    UDP_PORT = 65000      
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+DEVELOPMENT_MODE = True
+DELAY_IN_SECONDS = 1
+BROADCAST_IP = "255.255.255.255"
+PORT = 65000
 
-    # temperature = sense.get_temperature()
-    # pressure = sense.get_pressure()
-    # humidity = sense.get_humidity()
+if DEVELOPMENT_MODE:
+    from sense_HAT_emulator import SenseHatEmulator as SenseHat
 
-    # Test Data:   (Husk at udkommentere senere)
-    temperature = 17
-    pressure = 1000
-    humidity = 101
+    print("Running in DEVELOPMENT MODE. Using SenseHat emulator.")
+else:
+    try:
+        from sense_hat import SenseHat
 
-    # Create a dictionary with the sensor data        
-    sensor_data = {"Temperature": round(temperature, 2),"Humidity": round(humidity, 2), "Pressure": round(pressure, 2) }
-   
+        print("Running in PRODUCTION mode. Using real SenseHat.")
+    except ImportError as e:
+        print("Error: SenseHat library not found. Are you on a Raspberry Pi?")
+        raise e
+
+sense = SenseHat()
+
+with socket(AF_INET, SOCK_DGRAM) as sock:
+    sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
     # Broadcasting the sensor data to the network by using UDP, and serializing the data to JSON
-    try: 
-            
-        message = json.dumps(sensor_data)
-            
-        sock.sendto(message.encode('utf-8'), (UDP_IP, UDP_PORT))
-        # VI bruger følgende to linier til at teste om dataen bliver sendt og modtaget korrekt
-        # socket.SOL_SOCKET og socket.SO_BROADCAST gør at vi kan sende til broadcast adressen
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        print("Data sent:", message)   
-                
+    try:
+        while True:
+            temperature = sense.get_temperature()
+            pressure = sense.get_pressure()
+            humidity = sense.get_humidity()
+            sensor_data = {
+                "Temperature": round(temperature, 2),
+                "Humidity": round(humidity, 2),
+                "Pressure": round(pressure, 2),
+            }
+
+            message = json.dumps(sensor_data)
+
+            sock.sendto(message.encode("utf-8"), (BROADCAST_IP, PORT))
+            print("Data sent:", message)
+
+            time.sleep(DELAY_IN_SECONDS)
+
     except KeyboardInterrupt:
         print("Broadcast stopped.")
-    finally:
-        sock.close()
-
-    time.sleep(10)
-
