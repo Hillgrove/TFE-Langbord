284af1ca9679a4891ba748e3bb90a45f28fdfa1d
Jesper Højlund <github@hillgrove.dk>
2024-12-03 11:37:59 +0100

    feat: add Serial Number to model

--- Backend/SensorDataRepository/Models/SensorData.cs
+++ Backend/SensorDataRepository/Models/SensorData.cs
@@ -10,6 +10,7 @@
         public double? Temperature { get; set; }
         public double? Humidity { get; set; }
         public double? Pressure { get; set; }
+        public string SerialNumber { get; set; } = "UNKNOWN";
 
         //[ForeignKey("SensorUnit")]
         // public int? SensorUnitId { get; set; }
