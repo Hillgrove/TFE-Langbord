55bd3c5a8d6bed573ba1faf8fbeb2e665676ef93
Jesper Højlund <github@hillgrove.dk>
2024-12-04 21:45:46 +0100

    feat: add tests of SensorDataRepository (done)  and template for test of SensorRepositry (not done)

---
+++ Backend/DataAccessTests/Repositories/SensorDataRepositoryTests.cs
@@ -0,0 +1,107 @@
+﻿using DataAccess.Repositories;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataAccess.Tests
+{
+    [TestClass]
+    public class SensorDataRepositoryTests
+    {
+        private SensorDataRepository _repository = null!;
+        private AppDbContext _context = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new AppDbContext(options);
+            _repository = new SensorDataRepository(_context);
+
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+
+        [TestMethod]
+        public void Add_ShouldAddSensorData()
+        {
+            // Arrange
+            var sensorData = new SensorData
+            {
+                SensorId = 1,
+                Temperature = 25.0,
+                Humidity = 50.0,
+                Pressure = 1000.0,
+                Timestamp = DateTime.Now
+            };
+
+            // Act
+            var result = _repository.Add(sensorData);
+
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, _context.SensorData.Count());
+        }
+
+        [TestMethod]
+        public void GetAll_ShouldReturnAllSensorData()
+        {
+            // Arrange
+            var sensorData1 = new SensorData { SensorId = 1, Temperature = 25.0, Humidity = 50.0, Pressure = 1000.0 };
+            var sensorData2 = new SensorData { SensorId = 2, Temperature = 26.0, Humidity = 51.0, Pressure = 1001.0 };
+
+            _context.SensorData.AddRange(sensorData1, sensorData2);
+            _context.SaveChanges();
+
+            // Act
+            var result = _repository.GetAll();
+
+            // Assert
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        public void Get_ValidId_ReturnsCorrectSensorData()
+        {
+            var sensorData = new SensorData { SensorId = 1, Temperature = 25.0, Humidity = 50.0, Pressure = 1000.0, Timestamp = DateTime.Now };
+            _context.SensorData.Add(sensorData);
+            _context.SaveChanges();
+
+            var result = _repository.Get(sensorData.Id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(sensorData.Id, result.Id);
+        }
+
+        [TestMethod]
+        public void Get_InvalidId_ReturnsNull()
+        {
+            var sensorData = new SensorData { SensorId = 1, Temperature = 25.0, Humidity = 50.0, Pressure = 1000.0, Timestamp = DateTime.Now };
+            _context.SensorData.Add(sensorData);
+            _context.SaveChanges();
+
+            var result = _repository.Get(sensorData.Id + 1);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void DeleteOlderThan_CutoffDatePassed_DeletesOldSensorData()
+        {
+            var oldSensorData = new SensorData { SensorId = 1, Temperature = 25.0, Humidity = 50.0, Pressure = 1000.0, Timestamp = DateTime.Now.AddYears(-1) };
+            var newSensorData = new SensorData { SensorId = 2, Temperature = 26.0, Humidity = 51.0, Pressure = 1001.0, Timestamp = DateTime.Now };
+
+            _context.SensorData.AddRange(oldSensorData, newSensorData);
+            _context.SaveChanges();
+
+            var result = _repository.DeleteOlderThan(DateTime.Now.AddMonths(-6));
+
+            Assert.AreEqual(1, result);
+            Assert.AreEqual(1, _context.SensorData.Count());
+        }
+    }
+}
---
+++ Backend/DataAccessTests/Repositories/SensorRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Tests
+{
+    [TestClass()]
+    public class SensorRepositoryTests
+    {
+        [TestMethod()]
+        public void SensorRepositoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSensorDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
