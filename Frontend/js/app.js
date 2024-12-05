329a7660d9b0dcf85ff7261ba3096f0a7658d74c
yue001 <yue001@edu.zealand.dk>
2024-12-05 19:56:37 +0100

    frontend clean, refactor, added vue, js etc.

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -1,31 +1,88 @@
-const baseUrl = "https://www.7timer.info/bin/civillight.php?lon=12.0746571&lat=55.6240144&ac=0&unit=metric&output=json&tzshift=0"
-
 Vue.createApp({
     data() {
         return {
-            sensorData: null,
-            currentDate: null,
+
+            userLocation: {
+              city: "Roskilde",
+              lon: 12.0746571,
+              lat: 55.6240144
+            },
+
+            locations: [
+
+                {
+                    city: "Roskilde",
+                    lon: 12.0746571,
+                    lat: 55.6240144
+                },
+
+                {
+                    city: "Holbæk",
+                    lon: 11.6345319,
+                    lat: 55.7160127,
+                },
+
+                {
+                    city: "Køge",
+                    lon: 12.0806785,
+                    lat: 55.4468255,
+                },
+
+                {
+                    city: "Brøndby",
+                    lon: 12.3891614,
+                    lat: 55.6483768,
+                },
+
+                {
+                    city: "København",
+                    lon: 12.5526248,
+                    lat: 55.6713089,
+                }
+                ,
+                {
+                    city: "Istanbul",
+                    lon: 28.6825564,
+                    lat: 41.0053702,
+                },
+
+            ],
+
+            sensorData: "",
+            currentDate: "",
         }
     },
 
-    async created() {
-        this.getAll(baseUrl); // + 'sensor')
+    created() {
+        this.getCurrentWeather();
     },
 
     methods: {
 
-        async getAll(url) {
+        // Get Current Weather
+        async getCurrentWeather()   {
+
+            var requestUri = 'https://www.7timer.info/bin/civillight.php?lon='+ this.userLocation.lon +'&lat='+ this.userLocation.lat +'&ac=0&unit=metric&output=json&tzshift=0'
+
             try {
-                const response = await axios.get(url) // http://localhost:5207/api/sensor
+                const response = await axios.get(requestUri)
                 this.result = await response.data
                 this.sensorData = this.result.dataseries;
-
-                console.log(this.result);
-                console.log(this.sensorData);
-
             } catch (ex) {
                 alert(ex.message)
             }
+        },
+
+        changeUserLocation()
+        {
+            var selectionId = this.userLocationSelection;
+            var selection = this.locations[selectionId];
+            this.userLocation = selection;
+
+            this.getCurrentWeather();
+
         }
+
+
     }
 }).mount("#app")
