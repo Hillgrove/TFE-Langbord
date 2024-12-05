bf9b418ba125658cb0e30ab623705de514ef2305
Jesper Højlund <github@hillgrove.dk>
2024-12-05 12:06:23 +0100

    feat: add room repo

---
+++ Backend/DataAccess/Repositories/RoomRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Models;
+
+
+namespace DataAccess.Repositories
+{
+    public class RoomRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RoomRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Room Add(Room room)
+        {
+            _context.Rooms.Add(room);
+            _context.SaveChanges();
+
+            return room;
+        }
+
+        public IEnumerable<Room> GetAll()
+        {
+            return _context.Rooms.ToList();
+        }
+
+
+        public IEnumerable<SensorData> GetRecentSensorDataForRoom(int roomId, int days)
+        {
+            // find all sensor data for the specific room and within the last x days
+            var cutoffDate = DateTime.UtcNow.AddDays(-days);
+            return _context.SensorData
+                           .Where(sd => sd.Sensor.RoomId == roomId && sd.Timestamp >= cutoffDate)
+                           .ToList();
+        }
+
+        public Room? Get(int id)
+        {
+            return _context.Rooms.FirstOrDefault(r => r.Id == id);
+        }
+
+        public void Delete(int id)
+        {
+            var room = _context.Rooms.FirstOrDefault(r => r.Id == id);
+
+            if (room != null)
+            {
+                _context.Rooms.Remove(room);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
